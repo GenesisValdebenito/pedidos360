d10b01a13210e56f1c65518120378b96ec3ccb60
GenesisValdebenito <g.valdebenitoquintupil@gmail.com>
2026-09-23 18:40:36 -0300

    fix scope MSAL (2 App Registrations) + scaffolding manual de orders-service y catalog-service

---
+++ backend/catalog-service/src/test/java/com/pedidos360/catalog_service/CatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedidos360.catalog_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
